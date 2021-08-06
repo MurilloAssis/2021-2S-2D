2a6b2750d88ef563e33b43013a74a8d4ea6fc154
Murillo <murillo98asiss@gmail.com>
2021-08-06 14:12:24 -0300

    Exercicio 1.4 finalizado

--- 2T-sprint1-bd/exercícios/1.4-exercicio-optus/Scripts/DML 1.4.sql
+++ 2T-sprint1-bd/exercícios/1.4-exercicio-optus/Scripts/DML 1.4.sql
@@ -1,7 +1,21 @@
-USE USUARIO;
+USE OPTUS;
 GO
 
 INSERT INTO USUARIO(nomeUsuario, emailUsuario, senhaUsuario, tipoUsuario)
-VALUES ('ALBERTO', 'alberto@email.com', '1234', 'Admin')
+VALUES ('ALBERTO', 'alberto@email.com', '1234', 'Admin');
 
-SELECT * FROM USUARIO;
+INSERT INTO ARTISTA (nomeArtista)
+VALUES ('CantorLoco'), ('Roberto Barros');
+GO 
+
+INSERT INTO ESTILO (nomeEstilo)
+VALUES ('Rap'), ('FUNK');
+GO 
+
+INSERT INTO ALBUM (idArtista, nomeAlbum, dataLancamento)
+VALUES (1, 'RAPGANG', '10-07-2019'), (2, 'MANDELA', '20-07-2021');
+GO 
+
+INSERT INTO ESTILOALBUM (idAlbum, idEstilo)
+VALUES (1, 1), (2, 2);
+GO 
