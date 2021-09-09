323932b583900a3bbf16ebf9ba3f59fd8088f7ac
Gustavo <gumiguel8@hotmail.com>
2021-09-09 18:33:45 -0300

    domain e interfaces prontas

---
+++ 2T-sprint2-api/Exercícios/InLock/Back-end/senai.inlock.webApi/senai.inlock.webApi/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,45 @@
+﻿using senai.inlock.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{   
+    /// <summary>
+    /// Interface responsável pelo repositório EstudioRepository
+    /// </summary>
+    interface IEstudioRepository
+    {
+        /// <summary>
+        /// Cadastra um novo estúdio
+        /// </summary>
+        /// <param name="novoEstudio">Novo estúdio a ser cadastrado</param>
+        void Cadastrar(EstudioDomain novoEstudio);
+
+        /// <summary>
+        /// Lista todos os estúdios
+        /// </summary>
+        /// <returns>Uma lista de estúdios</returns>
+        List<EstudioDomain> ListarTodos();
+
+        /// <summary>
+        /// Busca um estúdio através do id
+        /// </summary>
+        /// <param name="idEstudio">Id do estúdio a ser procurado</param>
+        /// <returns>Um estúdio</returns>
+        EstudioDomain BuscarPorId(int idEstudio);
+
+        /// <summary>
+        /// Atualiza um estúdio existente
+        /// </summary>
+        /// <param name="idEstudio">Id do estúdio a ser atualizado</param>
+        void AtualizarId(int idEstudio);
+
+        /// <summary>
+        /// Deleta um estúdio existente
+        /// </summary>
+        /// <param name="idEstudio">Id do estúdio a ser deletado</param>
+        void Deletar(int idEstudio);
+    }
+}
---
+++ 2T-sprint2-api/Exercícios/InLock/Back-end/senai.inlock.webApi/senai.inlock.webApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,45 @@
+﻿using senai.inlock.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório UsuarioRepository
+    /// </summary>
+    interface IUsuarioRepository
+    {
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="novoUsuario">Novo usuário a ser cadastrado</param>
+        void Cadastrar(UsuarioDomain novoUsuario);
+
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuários</returns>
+        List<UsuarioDomain> ListarTodos();
+
+        /// <summary>
+        /// Busca um usuário através do id
+        /// </summary>
+        /// <param name="idUsuario">Id do usuário a ser procurado</param>
+        /// <returns>Um usuário</returns>
+        UsuarioDomain BuscarPorId(int idUsuario);
+
+        /// <summary>
+        /// Atualiza um usuário existente
+        /// </summary>
+        /// <param name="idUsuario">Id do usuário a ser atualizado</param>
+        void AtualizarId(int idUsuario);
+
+        /// <summary>
+        /// Deleta um usuário existente
+        /// </summary>
+        /// <param name="idUsuario">Id do usuário a ser deletado</param>
+        void Deletar(int idUsuario);
+    }
+}
