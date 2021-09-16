82a8cab07d013f6b4b5486903fade6baf1d25cc1
MurilloAssis <murillo98asiss@gmail.com>
2021-09-16 14:32:21 -0300

    Banco de dados atualizado

---
+++ 2T-sprint1-bd/Projeto 1 - HROADS/Scripts/hroads-DML.sql
@@ -0,0 +1,57 @@
+USE HROADS;
+GO
+
+/*
+3 - Inserir os registros conforme descrição no próprio texto (classes, habilidades,
+tipos de habilidades e personagens)
+*/
+
+INSERT INTO TIPOUSUARIO(titulo)
+VALUES ('Jogador'),('Administrador')
+
+INSERT INTO USUARIO(email, senha, idTipoUsuario)
+VALUES ('jogador@email.com', 123, 1), ('admin@email.com', 123, 2)
+
+INSERT INTO CLASSE (nomeClasse)
+VALUES	('Bárbaro'),('Cruzado'),('Caçadora de Demônios'),
+		('Monge'),('Necromante'),('Feiticeiro'),('Arcanista')
+GO
+
+INSERT INTO TIPOHABILIDADE (nomeTipoHabilidade)
+VALUES	('Ataque'),('Defesa'),('Cura'),('Magia')
+GO
+
+INSERT INTO PERSONAGEM (idClasse, nomePersonagem, capacidadeMaxVida,
+capacidadeMaxMana, dataAtualizacao, dataCriacao)
+VALUES	(1,'DeuBug',100,80,GETDATE(),'18/01/2019 17:35:21'),
+		(4,'BitBug',70,100,GETDATE(),'17/03/2016 09:45:03'),
+		(7,'Fer8',75,60,GETDATE(),'18/03/2018 16:02:01')
+GO
+
+INSERT INTO HABILIDADE (idTipoHabilidade, nomeHabilidade)
+VALUES	(1,'Lança Mortal'),
+		(2,'Escudo Supremo'),
+		(3,'Recuperar Vida')
+GO
+
+INSERT INTO CLASSEHABILIDADE (idClasse, idHabilidade)
+VALUES	(1,1),(1,2),(2,2),(3,1),(4,3),(4,2),(6,3)
+GO
+
+
+/*
+4 - Atualizar o nome do personagem Fer8 para Fer7
+*/
+UPDATE PERSONAGEM
+SET nomePersonagem = 'Fer7'
+WHERE nomePersonagem = 'Fer8'
+GO
+
+/*
+5 - Atualizar o nome da classe de Necromante para Necromancer
+*/
+UPDATE CLASSE 
+SET nomeClasse = 'Necromancer'
+WHERE nomeClasse = 'Necromante'
+GO
+
---
+++ 2T-sprint1-bd/Projeto 1 - HROADS/Scripts/hroads-DQL.sql
@@ -0,0 +1,82 @@
+USE HROADS;
+GO
+
+
+-- 6 - Selecionar todos os personagens
+
+
+
+SELECT * FROM PERSONAGEM
+GO
+
+-- 7 - Selecionar todos as classes
+SELECT * FROM CLASSE
+GO
+
+-- 8 - Selecionar somente o nome das classes
+SELECT nomeClasse FROM CLASSE
+GO
+
+-- 9 - Selecionar todas as habilidades
+SELECT * FROM HABILIDADE
+GO
+
+-- 10 - Realizar a contagem de quantas habilidades estão cadastradas
+SELECT COUNT(*) FROM HABILIDADE
+GO
+
+-- 11 - Selecionar somente os id’s das habilidades classificando-os por ordem crescente
+SELECT idHabilidade FROM HABILIDADE
+ORDER BY idHabilidade ASC
+GO
+
+-- 12 - Selecionar todos os tipos de habilidades
+SELECT * FROM TIPOHABILIDADE
+GO
+
+-- 13 - Selecionar todas as habilidades e a quais tipos de habilidades elas fazem parte
+SELECT nomeHabilidade Habilidade, nomeTipoHabilidade [Tipo Habilidade] FROM HABILIDADE H
+LEFT JOIN TIPOHABILIDADE TH
+ON H.idTipoHabilidade = TH.idTipoHabilidade
+GO
+
+-- 14 - Selecionar todos os personagens e suas respectivas classes
+SELECT nomePersonagem Personagem, nomeClasse Classe FROM PERSONAGEM P
+LEFT JOIN CLASSE C
+ON P.idClasse = C.idClasse
+GO
+
+/* 15 - Selecionar todos os personagens e as classes (mesmo que elas não tenham
+correspondência em personagens) */
+SELECT nomePersonagem Personagem, nomeClasse Classe FROM PERSONAGEM P
+RIGHT JOIN CLASSE C
+ON P.idClasse = C.idClasse
+GO
+
+-- 16 - Selecionar todas as classes e suas respectivas habilidades
+SELECT nomeClasse Classe, nomeHabilidade Habilidade FROM CLASSE C
+LEFT JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+LEFT JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
+
+/* 17 - Selecionar todas as habilidades e suas classes (somente as que possuem
+correspondência); */
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM CLASSE C
+LEFT JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+INNER JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
+
+/* 
+Selecionar todas as habilidades e suas classes (mesmo que elas não tenham
+correspondência).
+*/
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM CLASSE C
+FULL OUTER JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+FULL OUTER JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
