0ef7fa41f0afda94727cd316f883d032c00cb127
maatheuss0 <matheusaraujoms.machado@gmail.com>
2021-09-17 14:24:04 -0300

    CRUD Personagem finalizado

--- 2T-sprint2-api/Projetos/HROADS/Backend/senai_hroads_tarde_webapi/senai_hroads_tarde_webapi/Contexts/InLockContext.cs
+++ 2T-sprint2-api/Projetos/HROADS/Backend/senai_hroads_tarde_webapi/senai_hroads_tarde_webapi/Contexts/InLockContext.cs
@@ -31,7 +31,7 @@ namespace senai_hroads_tarde_webapi.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CV21P6P\\SQLEXPRESS; initial catalog=HROADS; user Id=sa; pwd=#Murillo1#;");
+                optionsBuilder.UseSqlServer("Data Source=NOTE0113A4\\SQLEXPRESS; initial catalog=HROADS; user Id=sa; pwd=Senai@132;");
             }
         }
 
