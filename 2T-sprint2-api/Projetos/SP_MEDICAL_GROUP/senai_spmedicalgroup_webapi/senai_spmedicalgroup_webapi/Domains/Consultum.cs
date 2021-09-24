641b69269545b7f37c0b01be9918f459a5a6b9bb
MurilloAssis <murillo98asiss@gmail.com>
2021-09-24 09:59:51 -0300

    Projeto SP Medical Group Iniciado

---
+++ 2T-sprint2-api/Projetos/SP_MEDICAL_GROUP/senai_spmedicalgroup_webapi/senai_spmedicalgroup_webapi/Domains/Consultum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_spmedicalgroup_webapi.Domains
+{
+    public partial class Consultum
+    {
+        public short IdConsulta { get; set; }
+        public short? IdMedico { get; set; }
+        public short? IdSituacao { get; set; }
+        public short? IdPaciente { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public string DescricaoConsulta { get; set; }
+
+        public virtual Medico IdMedicoNavigation { get; set; }
+        public virtual Paciente IdPacienteNavigation { get; set; }
+        public virtual Situacao IdSituacaoNavigation { get; set; }
+    }
+}
